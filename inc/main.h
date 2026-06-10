1a8c795e4dfc89afb77948fd998bdf156241a0df
annizzg666 <84681641@qq.com>
2026-06-10 16:40:04 +0800

    增加打印

--- inc/main.h
+++ inc/main.h
@@ -2,5 +2,6 @@
 #define __MAIN_H__
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #endif /* __MAIN_H__ */
--- src/main.c
+++ src/main.c
@@ -4,5 +4,8 @@
 int main()
 {
     printf("Hello, World!\n");
+    printf("This is a C++ project using the language standard version.\n");
+    printf("Feel free to modify and expand this project as needed.\n");
+    printf("Happy coding!\n");
     return 0;
 }
